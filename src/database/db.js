1584eca9ac3ed82f291b196907f72800b847316a
mohit639592 <mohitsharma639592@gmail.com>
2025-08-16 19:08:57 +0530

    update

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const connect = ()=>{
-    mongoose.connect("mongodb+srv://mohit:9aBCcL1jZBJupPOM@cluster0.safgwzo.mongodb.net/")
+    mongoose.connect("mongodb+srv://mohit:SPKFKYIszcLQ5I7d@cluster0.safgwzo.mongodb.net/")
     .then(()=>{
         console.log("DB CONNECTED");
 
